fcc78de59fcccf7b7cb8ab66b205fcdbff30fc31
Kevin Imvoo <imvooz@gmail.com>
2018-07-31 09:09:17 +1000

    Add final changes to code.

--- js/messagereceiver.js
+++ js/messagereceiver.js
@@ -1,7 +1,7 @@
 class MessageReceiver {
   constructor() {
     this.client = mqtt.connect(
-      "ws://localhost:3001",
+      "ws://192.168.43.76:3001",
       {
         username: "admin",
         password: "secretpassword",
@@ -13,12 +13,15 @@ class MessageReceiver {
 
     this.lastReceivedRot = {};
     this.lastReceivedQuat = {};
+    this.lastReceivedAccel = {};
 
     this.client.on("connect", () => {
-      this.client.subscribe("euler/98072d27a984");
-      this.client.subscribe("quat/98072d27a984");
+      this.client.subscribe("euler/98072d3b1a82");
+      this.client.subscribe("quat/98072d3b1a82");
+      this.client.subscribe("telemetry/98072d3b1a82/movement");
       this.client.subscribe("euler/c4be8471a302");
       this.client.subscribe("quat/c4be8471a302");
+      this.client.subscribe("telemetry/c4be8471a302/movement");
     });
 
     this.client.on("message", (topic, message) => {
@@ -32,6 +35,8 @@ class MessageReceiver {
         this.lastReceivedRot[deviceId] = new THREE.Euler(payload.pitch, payload.heading, payload.roll, "XYZ");
       } else if (topicTitle == "quat") {
         this.lastReceivedQuat[deviceId] = new THREE.Quaternion(payload.x, payload.y, payload.z, payload.w);
+      } else {
+        this.lastReceivedAccel[deviceId] = payload.accel;
       }
 
       this.messageReceivedListeners.forEach(listener => {
--- js/visualizer.js
+++ js/visualizer.js
@@ -3,6 +3,14 @@ class Visualizer {
     this.targetQuat = null;
     this.cubes = {};
 
+    this.SEPARATION = 100;
+    this.AMOUNTX = 50;
+    this.AMOUNTY = 50;
+    this.count = 0;
+
+    this.targetHighness = 0;
+    this.highness = 0;
+
     //
 
     let container = document.createElement("div");
@@ -11,14 +19,34 @@ class Visualizer {
     var windowHalfX = window.innerWidth / 2;
     var windowHalfY = window.innerHeight / 2;
 
-    let camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    let camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
     camera.position.y = 150;
-    camera.position.z = 500;
+    camera.position.z = 750;
     this.camera = camera;
 
     let scene = new THREE.Scene();
     scene.background = new THREE.Color(0xf0f0f0);
     this.scene = scene;
+
+    this.particles = new Array();
+    var PI2 = Math.PI * 2;
+    var material = new THREE.SpriteCanvasMaterial({
+      color: "black",
+      program: function(context) {
+        context.beginPath();
+        context.arc(0, 0, 0.5, 0, PI2, true);
+        context.fill();
+      }
+    });
+    var i = 0;
+    for (var ix = 0; ix < this.AMOUNTX; ix++) {
+      for (var iy = 0; iy < this.AMOUNTY; iy++) {
+        let particle = (this.particles[i++] = new THREE.Sprite(material));
+        particle.position.x = ix * this.SEPARATION - (this.AMOUNTX * this.SEPARATION) / 2;
+        particle.position.z = iy * this.SEPARATION - (this.AMOUNTY * this.SEPARATION) / 2;
+        this.scene.add(particle);
+      }
+    }
 
     // Plane
     let plane = new THREE.PlaneBufferGeometry(200, 200);
@@ -68,11 +96,67 @@ class Visualizer {
     this.cubes[cubeId].targetQuat = targetQuat;
   }
 
+  setTargetColour(hex) {
+    this.targetColour = new THREE.Color(hex);
+  }
+
+  generateRipple(xacc, yacc, zacc, colour = "gray") {
+    let particles = [];
+
+    var PI2 = Math.PI * 2;
+    var material = new THREE.SpriteCanvasMaterial({
+      color: colour,
+      program: function(context) {
+        context.beginPath();
+        context.arc(0, 0, 0.5, 0, PI2, true);
+        context.fill();
+      }
+    });
+
+    let seedx = Math.random() - 0.5;
+    let seedy = Math.random() - 0.5;
+
+    for (let i = 0; i < 5; i++) {
+      let particle = new THREE.Sprite(material);
+      particles.push(particle);
+
+      particle.position.set(
+        seedx * 0.5 * window.innerWidth + 15 * -xacc * (5 - i),
+        seedy * 0.5 * window.innerHeight + 15 * -yacc * (5 - i),
+        0.01
+      );
+      particle.scale.x = particle.scale.y = 0.001;
+
+      // console.log(seedy * 0.5 * window.innerHeight + 25 * -yacc * (10 - i));
+      let scale = Math.random() * 15 + 40;
+
+      new TWEEN.Tween(particle.scale)
+        .delay(200 * i)
+        .to({ x: scale, y: scale }, 500)
+        .onComplete(() => {
+          new TWEEN.Tween(particle.scale)
+            .to({ x: 0.001, y: 0.001 }, 400)
+            .onComplete(() => {
+              this.scene.remove(particle);
+            })
+            .start();
+        })
+        .start();
+
+      this.scene.add(particle);
+    }
+  }
+
+  setHighness(val) {
+    this.targetHighness = val;
+  }
+
   animate() {
     requestAnimationFrame(() => {
       this.animate();
     });
     this.render();
+    TWEEN.update();
   }
 
   render() {
@@ -100,6 +184,28 @@ class Visualizer {
 
       currentIndex += 1;
     }
+
+    // COLOURS
+    if (this.targetColour != null) {
+      let origColour = this.scene.background;
+      this.scene.background = origColour.lerp(this.targetColour, 0.03);
+    }
+    //
+
+    var i = 0;
+    for (var ix = 0; ix < this.AMOUNTX; ix++) {
+      for (var iy = 0; iy < this.AMOUNTY; iy++) {
+        let particle = this.particles[i++];
+        particle.position.y =
+          Math.sin((ix + this.count) * 0.3) * this.highness + Math.sin((iy + this.count) * 0.5) * this.highness;
+        particle.scale.x = particle.scale.y =
+          (Math.sin((ix + this.count) * 0.3) + 1) * 4 + (Math.sin((iy + this.count) * 0.5) + 1) * 4;
+      }
+    }
+
+    this.count += 0.1;
+
+    this.highness += (this.targetHighness - this.highness) * 0.02;
 
     this.renderer.render(this.scene, this.camera);
   }
